4d521edb94d4f6ad1d3f92824103397e7fa52193
Raj <raju.11811924@lpu.in>
2021-11-14 14:58:56 +0530

    Mathematics > Prime (more optimized)

--- Mathematics/Prime.java
+++ Mathematics/Prime.java
@@ -2,9 +2,12 @@ package Mathematics;
 
 public class Prime {
     public static boolean check(int num) {
-        if (num == 1) {
+        if (num == 1)
             return false;
-        }
+        if (num == 2 || num == 3)
+            return true;
+        if (num % 2 == 0 || num % 3 == 0)
+            return false;
 
         for (int i = 2; i * i <= num; i++) {
             if (num % i == 0) {
