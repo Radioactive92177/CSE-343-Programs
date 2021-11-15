28ca14602759df72c5d617eda3457ae1898b244f
Raj <raju.11811924@lpu.in>
2021-11-15 12:20:15 +0530

    Mathematics > Prime : Necessary Optimization

--- Mathematics/Prime.java
+++ Mathematics/Prime.java
@@ -9,8 +9,8 @@ public class Prime {
         if (num % 2 == 0 || num % 3 == 0)
             return false;
 
-        for (int i = 2; i * i <= num; i++) {
-            if (num % i == 0) {
+        for (int i = 5; i * i <= num; i += 6) {
+            if (num % i == 0 || num % (i + 2) == 0) {
                 return false;
             }
         }
